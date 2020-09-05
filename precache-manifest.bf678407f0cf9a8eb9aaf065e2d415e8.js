1bef7d9989f525940d77cb386aa3e1c2b284530f
unknown <pznamir00@gmail.com>
2020-09-05 21:29:17 +0200

    Updates

---
+++ precache-manifest.bf678407f0cf9a8eb9aaf065e2d415e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87411021ae34ec6978502b22f670150e",
+    "url": "/Visiting-card/index.html"
+  },
+  {
+    "revision": "e2676fbfae8f534268a3",
+    "url": "/Visiting-card/static/css/main.08fb10ba.chunk.css"
+  },
+  {
+    "revision": "7add6085d98115f895b1",
+    "url": "/Visiting-card/static/js/2.966ae02d.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Visiting-card/static/js/2.966ae02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2676fbfae8f534268a3",
+    "url": "/Visiting-card/static/js/main.6939eb00.chunk.js"
+  },
+  {
+    "revision": "7dd0eb9957af64f9b75f",
+    "url": "/Visiting-card/static/js/runtime-main.0de713ac.js"
+  }
+]);
